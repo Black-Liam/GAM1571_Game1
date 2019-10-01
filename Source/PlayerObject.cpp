e6043c2dd21a899350706ac1fbbed6ae58cf722b
Liam Black <blac0413@algonquinlive.com>
2019-10-01 10:34:10 -0400

    Saving Assignment 1 here before changing things for midterm

--- Source/PlayerObject.cpp
+++ Source/PlayerObject.cpp
@@ -37,12 +37,12 @@ void PlayerObject::Init()
     VertexFormat(-5.0f,     -5.0f,   0xff,   0xff,   0x00,   0xFF),
     VertexFormat(-5.0f,     5.0f,   0xff,   0xff,   0x00,   0xFF),
 
-    VertexFormat(-5.0f,     -15.0f,   0x00,   0x00,   0x80,   0xFF),
-    VertexFormat(5.0f,     -15.0f,   0x00,   0x00,   0x80,   0xFF),
-    VertexFormat(5.0f,     5.0f,   0x00,   0x00,   0x80,   0xFF),
-    VertexFormat(-5.0f,     -15.0f,   0x00,   0x00,   0x80,   0xFF),
-    VertexFormat(-5.0f,     5.0f,   0x00,   0x00,   0x80,   0xFF),
-    VertexFormat(5.0f,     5.0f,   0x00,   0x00,   0x80,   0xFF),
+    VertexFormat(-5.0f,     -15.0f,   0x00,   0xbc,   0x8a,   0xFF),
+    VertexFormat(5.0f,     -15.0f,   0xf0,   0xf7,   0x09,   0xFF),
+    VertexFormat(5.0f,     5.0f,   0xeb,   0x34,   0x0a,   0xFF),
+    VertexFormat(-5.0f,     -15.0f,   0x00,   0xbc,   0x8a,   0xFF),
+    VertexFormat(-5.0f,     5.0f,   0x7e,   0x07,   0x85,   0xFF),
+    VertexFormat(5.0f,     5.0f,   0xeb,   0x34,   0x0a,   0xFF),
 
     };
     numberOfVerts = sizeof(Attribs) / sizeof(VertexFormat);
@@ -52,7 +52,27 @@ void PlayerObject::Init()
 
 void PlayerObject::Update(float deltaTime, fw::Framework* p_Frame)
 {
-    if (p_Frame->IsKeyDown(VK_UP))
+    if (p_Frame->IsKeyDown(VK_UP)&& p_Frame->IsKeyDown(VK_LEFT))
+    {
+        m_up += deltaTime * speed * (float)sqrt(0.5);
+        m_right -= deltaTime * speed * (float)sqrt(0.5);
+    }
+    else if (p_Frame->IsKeyDown(VK_UP) && p_Frame->IsKeyDown(VK_RIGHT))
+    {
+        m_up += deltaTime * speed * (float)sqrt(0.5);
+        m_right += deltaTime * speed * (float)sqrt(0.5);
+    }
+    else if(p_Frame->IsKeyDown(VK_DOWN) && p_Frame->IsKeyDown(VK_LEFT))
+    {
+        m_up -= deltaTime * speed * (float)sqrt(0.5);
+        m_right -= deltaTime * speed * (float)sqrt(0.5);
+    }
+    else if(p_Frame->IsKeyDown(VK_DOWN) && p_Frame->IsKeyDown(VK_RIGHT))
+    {
+        m_up -= deltaTime * speed * (float)sqrt(0.5);
+        m_right += deltaTime * speed * (float)sqrt(0.5);
+    }
+    else if (p_Frame->IsKeyDown(VK_UP))
     {
         m_up += deltaTime * speed;
     }
@@ -68,6 +88,7 @@ void PlayerObject::Update(float deltaTime, fw::Framework* p_Frame)
     {
         m_right += deltaTime * speed;
     }
+
     if (p_Frame->IsKeyDown(VK_SPACE))
     {
         m_up = 0.0f;
